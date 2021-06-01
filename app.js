03f096915f6f321892e2df1f060f9d791f475506
MiheerTamkhane <54612063+MiheerTamkhane@users.noreply.github.com>
2021-06-01 21:38:42 +0530

    Banana talk ready
    Completed banana talk

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const button = document.querySelector("#btn-translate");
 const textArea = document.querySelector("#text-area");
 const output = document.querySelector(".output");
+
+serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getURL(text) {
+  return serverURL + "?text=" + text;
+}
+
+function errHandler(err) {
+  console.log("Error occured:" + err);
+  alert("something went wrong ;(");
+}
+
 function clickHandler() {
-  output.innerText = "blah " + textArea.value;
+  let inputText = textArea.value;
+
+  //getting info from server by doing fetch call
+  fetch(getURL(inputText))
+    .then((res) => res.json())
+    .then((data) => {
+      output.innerText = data.contents.translated;
+    })
+    .catch(errHandler);
 }
 
 button.addEventListener("click", clickHandler);
